28fa2fb74531690fd2079633a4bf33179ad8019b
rochi88 <raisul.me@gmail.com>
2026-04-07 09:33:14 +0600

    update command

--- src/Commands/CartCommand.php
+++ src/Commands/CartCommand.php
@@ -4,17 +4,32 @@ declare(strict_types = 1);
 
 namespace Centrex\Cart\Commands;
 
+use Centrex\Cart\Facades\Cart;
 use Illuminate\Console\Command;
 
 class CartCommand extends Command
 {
-    public $signature = 'laravel-cart';
+    public $signature = 'cart:clear
+                        {--instance=default : Cart instance to clear}';
 
-    public $description = 'My command';
+    public $description = 'Clear all items from a cart instance';
 
     public function handle(): int
     {
-        $this->comment('All done');
+        $instance = (string) $this->option('instance');
+
+        $cart = Cart::instance($instance);
+        $count = $cart->count();
+
+        if ($count === 0) {
+            $this->info("Cart instance [{$instance}] is already empty.");
+
+            return self::SUCCESS;
+        }
+
+        $cart->clear();
+
+        $this->info("Cleared {$count} item(s) from cart instance [{$instance}].");
 
         return self::SUCCESS;
     }
